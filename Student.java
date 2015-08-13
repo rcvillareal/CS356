948386b9cc7e3fb7d66e9e578e5babd3c26383bb
rcvillareal <rodney.villareal@gmail.com>
2015-08-13 00:33:59 -0700

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,15 @@
+/** Rodney Villareal
+ *  Project 1 CS 356
+ *  Student: Student class that sets
+ *  and gets a String for a student's id.
+ */
+
+public class Student {
+	private String id;
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getId() {
+		return id;
+	}
+}
